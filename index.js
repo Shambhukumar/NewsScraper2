960acbd7a777f662829dd0428e9399542f078bab
shambhu <shambhukumar.shau@gmail.com>
2026-03-31 11:30:18 +0530

    small cors import fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const mongoose = require('mongoose');
